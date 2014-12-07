24fb1ee9b5a18c40751c47d3f9fcf60a8ddb13e5
hkelf <hugo.kelfani@gmail.com>
2014-12-07 14:03:11 +0100

    have a better echo propagation

--- script/modules/Module_Radar.js
+++ script/modules/Module_Radar.js
@@ -27,45 +27,56 @@
 
     game.Module.define('module_radar')
         .data({
-            range: 0,
-            maxRange: 100,
+            rangeSize: 3,
+            rangeIn: 0,
+            rangeOut: 0,
+            maxRange: 120,
+            initialColdDown: 90,
+            coldDown: 0
         })
         .onUpdate(function (e, screen) {
             var player, map, nodeScannable, distX, distY,
                 dist, rotation, cosRotation, sinRotation, 
                 playerXCenter, playerYCenter, radarScreen;
 
-            player = screen.getEntity('entity_player', 'player');
-            map = screen.getEntity('entity_map', 'map');
-            radarScreen = screen.getEntity('entity_radarScreen', 'radarScreen');
+            if (this.coldDown === 0) {
+                player = screen.getEntity('entity_player', 'player');
+                map = screen.getEntity('entity_map', 'map');
+                radarScreen = screen.getEntity('entity_radarScreen', 'radarScreen');
 
-            if (this.range >= this.maxRange) {
-                this.range = 0;
-            } else {
-                this.range+=1;
-            }
+                if (player && map && radarScreen) {
+                    rotation = player.module('module_physics').rotation + Math.PI / 2;
+                    cosRotation = Math.cos(-rotation);
+                    sinRotation = Math.sin(-rotation);
+                    nodeScannable = map.firstChildNode;
+                    playerXCenter = player.xCenter;
+                    playerYCenter = player.yCenter;
 
-            if (player && map && radarScreen) {
-                rotation = player.module('module_physics').rotation + Math.PI / 2;
-                cosRotation = Math.cos(-rotation);
-                sinRotation = Math.sin(-rotation);
-                nodeScannable = map.firstChildNode;
-                playerXCenter = player.xCenter;
-                playerYCenter = player.yCenter;
+                    while (nodeScannable) {
+                        distX = nodeScannable.o.xCenter - playerXCenter;
+                        distY = nodeScannable.o.yCenter - playerYCenter;
+                        dist = Math.sqrt(distX * distX + distY * distY);
 
-                while (nodeScannable) {
-                    distX = nodeScannable.o.xCenter - playerXCenter;
-                    distY = nodeScannable.o.yCenter - playerYCenter;
-                    dist = Math.sqrt(distX * distX + distY * distY);
-
-                    if (~~dist === this.range) {
-                        parameters.type = nodeScannable.o.module('module_type').type;
-                        parameters.x = distX * cosRotation - distY * sinRotation + 400;
-                        parameters.y = distX * sinRotation + distY * cosRotation + 300;
-                        radarScreen.addChild('entity_pointOnRadar', parameters);
+                        if (~~dist >= this.rangeIn && ~~dist < this.rangeOut) {
+                            parameters.type = nodeScannable.o.module('module_type').type;
+                            parameters.x = distX * cosRotation - distY * sinRotation + 400;
+                            parameters.y = distX * sinRotation + distY * cosRotation + 300;
+                            radarScreen.addChild('entity_pointOnRadar', parameters);
+                        }
+                        nodeScannable = nodeScannable.next;
                     }
-                    nodeScannable = nodeScannable.next;
+                    
+                    if (this.rangeOut >= this.maxRange) {
+                        this.rangeOut = this.rangeSize;
+                        this.rangeIn = 0;
+                        this.coldDown = this.initialColdDown;
+                    } else {
+                        this.rangeOut += this.rangeSize;
+                        this.rangeIn += this.rangeSize;
+                    }
                 }
+            } else {
+                this.coldDown --;
             }
         });
 })()
